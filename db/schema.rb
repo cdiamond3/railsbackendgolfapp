4a9796cddeb25c8371a1f9760cfd833827fa83f0
cdiamond3 <diamond.chris.3@gmail.com>
2021-10-28 11:30:41 -0600

    posts table is set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_005041) do
+ActiveRecord::Schema.define(version: 2021_10_28_172957) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.string "input"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "input"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
